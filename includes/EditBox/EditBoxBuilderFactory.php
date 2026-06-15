7b2c70fcd6baab6c0e7d47183e3caa8f83fc3fcf
TehKittyCat <tehkittycat@weirdgloop.org>
2026-06-15 01:12:47 -0400

    Fix PHP notice

--- includes/EditBox/EditBoxBuilderFactory.php
+++ includes/EditBox/EditBoxBuilderFactory.php
@@ -26,6 +26,9 @@ class EditBoxBuilderFactory {
 	/** @var bool */
 	private $isCodeEditorLoaded;
 
+	/** @var bool */
+	private $isCodeMirrorLoaded;
+
 	/**
 	 * @param AbuseFilterPermissionManager $afPermManager
 	 * @param KeywordsManager $keywordsManager
