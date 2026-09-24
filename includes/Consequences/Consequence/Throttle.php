fd4b2490a6c651a5422ecb4a21525e49c4dae038
alex4401 <gerrit@parvos.net>
2026-09-24 20:27:06 +0100

    Throttle: Global filters should always use a global counter  key
    The throttle consequence varies its counter stash keys depending on
    whether current wiki hosts global filters or not, making central
    separate from other sites.

--- includes/Consequences/Consequence/Throttle.php
+++ includes/Consequences/Consequence/Throttle.php
@@ -148,7 +148,7 @@ class Throttle extends Consequence implements ConsequencesDisablerConsequence {
 		$identifier = sha1( implode( ':', $identifiers ) );
 
 		// TODO: Migration strategy to abusefilter-throttle keygroup
-		if ( $this->parameters->getIsGlobalFilter() && !$this->filterIsCentral ) {
+		if ( $this->parameters->getIsGlobalFilter() ) {
 			return $this->mainStash->makeGlobalKey(
 				'abusefilter', 'throttle', $this->centralDB, $this->throttleParams['id'], $identifier
 			);
